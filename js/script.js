8361af020f0df701dcd832053d41dce74fce2c79
Jonathan <jotah1313ar@gmail.com>
2024-07-05 17:15:43 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    fetch('/js/users.json')
+    fetch('/coder/js/users.json')
         .then(response => response.json())
         .then(users => {
             const user = users.find(user => user.username === username && user.password === password);
