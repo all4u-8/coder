9a1a0b5221f31c4c60bf9aa2898a8efee3175caf
all4u-8 <jotah1313ar@gmail.com>
2024-07-05 15:17:54 -0400

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,28 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    fetch('/js/users.json')
+        .then(response => response.json())
+        .then(users => {
+            const user = users.find(user => user.username === username && user.password === password);
+            
+            const messageDiv = document.getElementById('message');
+
+            if (user) {
+                messageDiv.style.color = '#c9ada7';
+                messageDiv.textContent = '¡Inicio de sesión exitoso!';
+                setTimeout(() => {
+                    window.location.href = '/pagina/1.html'; // Redirigir a la página específica
+                }, 1000); // Redirigir después de 1 segundo
+            } else {
+                messageDiv.style.color = 'red';
+                messageDiv.textContent = 'Usuario o contraseña incorrectos';
+            }
+        })
+        .catch(error => {
+            document.getElementById('message').textContent = 'Error al cargar los datos de usuario';
+        });
+});
